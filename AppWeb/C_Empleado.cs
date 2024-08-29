7c9c17e0394fc5c86a68e4f2e00d48f946ea5bdb
Raúl Alfaro <125586767+RaJami1205@users.noreply.github.com>
2024-08-29 00:03:58 -0600

    Update

---
+++ AppWeb/C_Empleado.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppWeb
+{
+    public class EmpleadoModel : PageModel
+    {
+        [BindProperty]
+        public int cedula { get; set; }
+
+        [BindProperty]
+        public string nombre { get; set; }
+
+        [BindProperty]
+        public string apellido1 { get; set; }
+
+        [BindProperty]
+        public string apellido2 { get; set; }
+
+        [BindProperty]
+        public int telefono { get; set; }
+
+        public void OnGet()
+        {
+            // Aquí puedes poner código para la carga inicial de la página, si es necesario.
+        }
+
+        public IActionResult OnPostRegistrar_empleado()
+        {
+
+            return RedirectToPage("Confirmacion");
+        }
+    }
+}
---
+++ AppWeb/C_Proyecto.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public string NombrePortafolio { get; set; }
+
+    [BindProperty]
+    public string Nombre { get; set; }
+
+    [BindProperty]
+    public int Año { get; set; }
+
+    [BindProperty]
+    public string Trimestre { get; set; }
+
+    [BindProperty]
+    public string Descripcion { get; set; }
+
+    [BindProperty]
+    public string Tipo { get; set; }
+
+    [BindProperty]
+    public DateTime FechaInicio { get; set; }
+
+    [BindProperty]
+    public DateTime FechaCierre { get; set; }
+
+    public void OnGet()
+    {
+        // Aquí puedes poner código para la carga inicial de la página, si es necesario.
+    }
+
+    public IActionResult OnPostRegistrar_proye()
+    {
+        
+        return RedirectToPage("Confirmacion");
+    }
+}
