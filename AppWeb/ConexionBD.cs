64bb7a0c2d68f9f568b73f7dd53708cb19bb6b20
jimendezm <132164589+jimendezm@users.noreply.github.com>
2024-08-30 01:56:17 -0600

    conexion update

--- AppWeb/ConexionBD.cs
+++ AppWeb/ConexionBD.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿
+
 using System.Data;
 using System.Data.SqlClient;
-using System.Configuration;
 
-
+namespace AppWeb;
 public class ConexionBD{
     private string stringConnection;
+    public List<>
 
-    public ConexionBD(){
-        stringConnection= ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+    public ConexionBD()
+    {
+        stringConnection = "Data Source=.\\mysqlserver;Initial Catalog=GestionProyectosTareas;Persist Security Info=True;User ID=sa;Password=***********;Trust Server Certificate=True";
+    }
+
+    // Método para cargar datos de una tabla
+    public void CargarDatos(string nombreTabla)
+    {
+        DataTable dataTable = new DataTable();
+        using (SqlConnection connection = new SqlConnection(stringConnection)){
+            string query = $"SELECT * FROM {nombreTabla}";
+            using (SqlCommand command = new SqlCommand(query, connection)){//Ejecutamos el codigo SQL
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader()){
+                    dataTable.Load(reader);
+                }
+            }
+        }
     }
 }
