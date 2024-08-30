29bae918c4f7f9e1ed50a72952ac1d3af869dcdb
jimendezm <132164589+jimendezm@users.noreply.github.com>
2024-08-29 20:37:59 -0600

    Update

---
+++ AppWeb/ConexionBD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+public class ConexionBD{
+    private string stringConnection;
+
+    public ConexionBD(){
+        stringConnection= ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+    }
+}
